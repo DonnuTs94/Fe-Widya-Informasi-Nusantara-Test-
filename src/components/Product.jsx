179007423b3b191754e3e7f84aa8d9e72fa222fb
DonnuTs69 <111718022+DonnuTs69@users.noreply.github.com>
2024-12-16 12:07:38 +0700

    adding create product

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,19 +1,89 @@
-/* eslint-disable no-unused-vars */
-import { Avatar, Box, Button, IconButton, Typography } from "@mui/material"
-import { IoIosAddCircleOutline } from "react-icons/io"
+import {
+  Avatar,
+  Box,
+  Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogTitle,
+  FormControl,
+  IconButton,
+  Input,
+  InputLabel,
+  MenuItem,
+  Select,
+  TextField,
+  Typography,
+} from "@mui/material"
+import { IoIosAddCircleOutline, IoMdClose } from "react-icons/io"
 import axiosInstance from "../configs/api"
-import { useEffect, useState } from "react"
+import { useEffect, useRef, useState } from "react"
 import { DataGrid } from "@mui/x-data-grid"
 import { HiDotsVertical } from "react-icons/hi"
+import { MdOutlineFileUpload } from "react-icons/md"
+import "react-quill/dist/quill.snow.css" // Snow theme CSS
+import ReactQuill from "react-quill"
 
 const Product = () => {
   const [products, setProducts] = useState([])
   // const [page, setPage] = useState(1)
   const [paginationModel, setPaginationModel] = useState({
     page: 0,
+    pageSize: 5,
   })
   const [pageSize, setPageSize] = useState(null)
   const [totalData, setTotalData] = useState(0)
+  const [openModal, setOpenModal] = useState(false)
+  const [formData, setFormData] = useState({
+    name: "",
+    category: "",
+    price: "",
+    quantity: "",
+    description: "",
+    image: "",
+  })
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+
+    try {
+      const formDataToSend = new FormData()
+
+      formDataToSend.append("name", formData.name)
+      formDataToSend.append("price", formData.price)
+      formDataToSend.append("quantity", formData.quantity)
+      formDataToSend.append("category", formData.category)
+      formDataToSend.append("description", formData.description)
+      formDataToSend.append("image", formData.image)
+
+      const response = await axiosInstance.post("product", formDataToSend)
+      console.log(response)
+    } catch (err) {
+      console.log(err)
+    }
+  }
+  const handleChange = (e) => {
+    const { name, value } = e.target
+
+    setFormData((prevFromData) => ({
+      ...prevFromData,
+      [name]: value,
+    }))
+  }
+
+  const handleFileChange = (e) => {
+    const file = e.target.files[0]
+    console.log(file)
+
+    setFormData((prevFromData) => ({
+      ...prevFromData,
+      image: file,
+    }))
+  }
+
+  console.log(formData)
+
+  const inputFileRef = useRef()
 
   const getProductData = async () => {
     try {
@@ -23,6 +93,8 @@ const Product = () => {
       setProducts(response.data.data)
       setPageSize(response.data.pageSize)
       setTotalData(response.data.totalItems)
+
+      console.log(response)
     } catch (err) {
       console.log(err)
     }
@@ -32,6 +104,15 @@ const Product = () => {
     // console.log("Fetching data for page:", page)
     getProductData()
   }, [paginationModel])
+
+  const handleOpenModal = () => {
+    setOpenModal(true)
+  }
+
+  const handleCloseModal = () => {
+    setOpenModal(false)
+  }
+
   const columns = [
     {
       field: "image",
@@ -90,6 +171,7 @@ const Product = () => {
           <Button
             variant="contained"
             sx={{ color: "white", bgcolor: "green", gap: "10px" }}
+            onClick={handleOpenModal}
           >
             <IoIosAddCircleOutline /> Add Product
           </Button>
@@ -115,12 +197,164 @@ const Product = () => {
               pageSize={pageSize}
               rowCount={totalData}
               paginationMode="server"
+              // onPageChange={(newPage) => {
+              //   console.log("New Page Index:", newPage) // Debug
+              //   setPage(newPage + 1) // Convert zero-based index to one-based index
+              // }}
+              // onPageSizeChange={(newSize) => {
+              //   console.log("Page Size Changed:", newSize) // Debug
+              //   setPageSize(newSize)
+              // }}
               onPaginationModelChange={setPaginationModel}
               autoPageSize
+              // autoHeight
             />
           </Box>
         </Box>
       </Box>
+
+      <Dialog open={openModal} onClose={handleCloseModal} maxWidth="lg">
+        <DialogTitle sx={{ m: 0, p: 2 }} id="customized-dialog-title">
+          Create New Product
+        </DialogTitle>
+        <IconButton
+          aria-label="close"
+          onClick={handleCloseModal}
+          sx={(theme) => ({
+            position: "absolute",
+            right: 8,
+            top: 8,
+            color: theme.palette.grey[500],
+          })}
+        >
+          <IoMdClose />
+        </IconButton>
+        <DialogContent dividers>
+          <Box
+            sx={{
+              display: "grid",
+              gridTemplateColumns: "repeat(2, 1fr)", // 2-column grid
+              gap: "15px",
+              mb: "15px",
+            }}
+          >
+            <TextField
+              label="Product Name"
+              name="name"
+              sx={{
+                "& .MuiOutlinedInput-notchedOutline": {
+                  borderColor: "black",
+                },
+                width: "auto",
+              }}
+              onChange={handleChange}
+              value={formData.name}
+            />
+            <TextField
+              label="Price"
+              type="number"
+              name="price"
+              value={formData.price}
+              onChange={handleChange}
+              sx={{
+                "& .MuiOutlinedInput-notchedOutline": {
+                  borderColor: "black",
+                },
+                width: "auto",
+              }}
+            />
+            <TextField
+              label="quantity"
+              type="number"
+              name="quantity"
+              value={formData.quantity}
+              onChange={handleChange}
+              sx={{
+                "& .MuiOutlinedInput-notchedOutline": {
+                  borderColor: "black",
+                },
+                width: "auto",
+              }}
+            />
+            <FormControl>
+              <InputLabel>Category</InputLabel>
+              <Select
+                label="Category"
+                sx={{
+                  "& .MuiOutlinedInput-notchedOutline": {
+                    borderColor: "black",
+                  },
+                  width: "auto",
+                }}
+                name="category"
+                value={formData.category}
+                onChange={handleChange}
+              >
+                <MenuItem value="PRODUCT_A">Product A</MenuItem>
+                <MenuItem value="PRODUCT_B">Product B</MenuItem>
+                <MenuItem value="PRODUCT_C">Product C</MenuItem>
+                <MenuItem value="PRODUCT_D">Product D</MenuItem>
+              </Select>
+            </FormControl>
+          </Box>
+
+          <ReactQuill
+            theme="snow" // The "snow" theme is the default
+            value={formData.description} // Content of the editor
+            onChange={(content) =>
+              setFormData({ ...formData, description: content })
+            } // Handle content changes
+            placeholder="Write something here..."
+            style={{ height: "200px", marginBottom: "50px" }} // Customize editor height
+          />
+
+          <Box
+            sx={{
+              gridColumn: "span 2", // Spans across both columns
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+              justifyContent: "center",
+              gap: 2, // Space between the buttons
+              mt: "20px",
+            }}
+          >
+            <Button
+              component="label"
+              variant="outlined"
+              startIcon={<MdOutlineFileUpload />}
+              sx={{
+                color: "black",
+                border: "none",
+              }}
+            >
+              Upload file
+              <Input
+                type="file"
+                inputProps={{ multiple: true }}
+                sx={{ display: "none" }}
+                ref={inputFileRef}
+                required
+                onChange={handleFileChange}
+              />
+            </Button>
+            <Button
+              sx={{ width: "30%" }}
+              color="success"
+              variant="contained"
+              type="submit"
+              onClick={handleSubmit}
+            >
+              Submit
+            </Button>
+          </Box>
+        </DialogContent>
+        <DialogActions>
+          <Button autoFocus onClick={handleCloseModal}>
+            Save changes
+          </Button>
+        </DialogActions>
+      </Dialog>
     </>
   )
 }
